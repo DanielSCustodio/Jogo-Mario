0a6e0b4550927f0de60dca92d75e90d71bdec2e2
Daniel Custodio <play.daniel@hotmail.com>
2022-07-13 21:13:06 -0300

    feat: Sistema de pontuação criado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const mario = document.getElementById('mario');
 const pipe = document.getElementById('pipe');
 const clouds = document.getElementById('clouds');
+const textGameOver = document.getElementById('text-game-over');
+const btnRestart = document.getElementById('btn-restart');
+const score = document.getElementById('score');
+const screen = document.getElementById('game-board');
 
 const jump = () => {
   mario.classList.add('jump');
@@ -9,12 +13,15 @@ const jump = () => {
   }, 500)
 }
 
+const restart = () => {
+  document.location.reload(true);
+}
+
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft;
   const cloudsPosition = clouds.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-  console.log(cloudsPosition);
   if(
     pipePosition > 0 &&
     pipePosition <= 120 &&
@@ -31,11 +38,28 @@ const loop = setInterval(() => {
     clouds.style.left =`${cloudsPosition}px`
 
     mario.src = './images/game-over.png';
-    mario.style.width = '75px';
-    mario.style.marginLeft= '50px';
+    mario.style.width = '50px';
+    mario.style.marginLeft= '75px';
     
+    textGameOver.classList.remove('game-over-hidden')
+    textGameOver.classList.add('text-game-over');
+
+    btnRestart.classList.remove('btn-game-over-hidden')
+    btnRestart.classList.add('btn-restart');
+    
+    document.addEventListener('click', restart)
+
     clearInterval(loop)
+  }else {
+    let punctation = (setTimeout(()=>{},0))
+    score.innerHTML= punctation;
+    if(punctation == 100) {
+      screen.style.backgroundColor = 'blue' ;
+    }
   }
 }, 10)
+
+
+
 
 document.addEventListener('keydown', jump)
